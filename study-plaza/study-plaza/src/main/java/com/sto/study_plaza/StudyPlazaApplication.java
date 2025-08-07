28b3dc9a1f381354ea08cd479e0e787df34e9261
HTH016 <hthth01116@gmail.com>
2025-08-07 23:16:53 +0900

    #000 Initial commit

---
+++ study-plaza/study-plaza/src/main/java/com/sto/study_plaza/StudyPlazaApplication.java
@@ -0,0 +1,13 @@
+package com.sto.study_plaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPlazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPlazaApplication.class, args);
+	}
+
+}
---
+++ study-plaza/study-plaza/src/test/java/com/sto/study_plaza/StudyPlazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sto.study_plaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
